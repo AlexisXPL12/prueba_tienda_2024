acc34513eb85a285c3d8c39d3e6c929f993c8745
AlexisXPL12 <valdiviatruciosalexisgabriel@gmail.com>
2024-10-10 11:05:10 -0500

    12

--- models/vistas_model.php
+++ models/vistas_model.php
@@ -4,7 +4,7 @@ class vistaModelo{
     protected static function obtener_vistas($vista)
     {
         $palabras_permitidas = ['usuarios','usuario','usuario-modificar', 'productos','producto-modificar','producto','inicio','carrito',
-                                'tienda','contactar','tienda-producto','servicios','deseados','perfil'];
+                                'tienda','contactar','tienda-producto','servicios','deseados','perfil','registrarse','cursos','tienda-p-select'];
         if (in_array($vista, $palabras_permitidas)) {
             if (is_file("./views/" . $vista . ".php")) {
                 $contenido = "./views/" . $vista . ".php";
